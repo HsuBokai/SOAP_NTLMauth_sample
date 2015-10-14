9c958f7a8424baea6d9007e0f67fd2044b484592
HsuBokai <exp2.718281@gmail.com>
2015-10-14 14:14:54 +0800

    A visual c# project tring to access web services with SOAP and NTLM authentication.

---
+++ digiEAR_API_SOAP_sample/digiEAR_API_SOAP_sample/DE_API.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+using System.Net;
+using System.Windows.Forms;
+using System.IO;
+using System.Data;
+
+namespace digiEAR_API_SOAP_sample
+{
+    class DE_API
+    {
+        string site;
+        string userName;
+        string password;
+
+        public DE_API(string s = "localhost", string u = "digiEARuser", string p = "1234")
+        {
+            site = s;
+            userName = u;
+            password = p;
+        }
+
+        public int digiEAR_indexAllowed()
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string ret_str = CallWebService("http://" + site + "/digiEARservices/DEindex.asmx", "digiEAR_indexAllowed", parameters, new NetworkCredential(userName, password, site));
+            if (ret_str == "")
+            {
+                return -1;
+            }
+            else
+            {
+                XmlDocument res_doc = new XmlDocument();
+                res_doc.LoadXml(ret_str);
+                return Convert.ToInt32(res_doc.DocumentElement.GetElementsByTagName("digiEAR_indexAllowedResult").Item(0).InnerText);
+            }
+        }
+
+        public string digiEAR_indexOpenHandle(string acoName, out int hIndex)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("acoName", acoName);
+            string ret_str = CallWebService("http://" + site + "/digiEARservices/DEindex.asmx", "digiEAR_indexOpenHandle", parameters, new NetworkCredential(userName, password, site));
+            if (ret_str == "")
+            {
+                hIndex = 0;
+                return "web_services_error";
+            }
+            else
+            {
+                XmlDocument res_doc = new XmlDocument();
+                res_doc.LoadXml(ret_str);
+                hIndex = System.Int32.Parse(res_doc.DocumentElement.GetElementsByTagName("ihIndex").Item(0).InnerText);
+                return res_doc.DocumentElement.GetElementsByTagName("digiEAR_indexOpenHandleResult").Item(0).InnerText;
+            }
+        }
+
+        public string digiEAR_index(string medFile, string acoName, string patDir, int hIndex)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("freshFile", medFile);
+            parameters.Add("acoName", acoName);
+            parameters.Add("targetDir", patDir);
+            parameters.Add("ihStream", hIndex.ToString());
+            string ret_str = CallWebService("http://" + site + "/digiEARservices/DEindex.asmx", "digiEAR_index", parameters, new NetworkCredential(userName, password, site));
+            if (ret_str == "")
+            {
+                return "web_services_error";
+            }
+            else
+            {
+                XmlDocument res_doc = new XmlDocument();
+                res_doc.LoadXml(ret_str);
+                return res_doc.DocumentElement.GetElementsByTagName("digiEAR_indexResult").Item(0).InnerText;
+            }
+        }
+
+        public string digiEAR_indexCloseHandle(int hIndex)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("ihIndex", hIndex.ToString());
+            string ret_str = CallWebService("http://" + site + "/digiEARservices/DEindex.asmx", "digiEAR_indexCloseHandle", parameters, new NetworkCredential(userName, password, site));
+            if (ret_str == "")
+            {
+                return "web_services_error";
+            }
+            else
+            {
+                XmlDocument res_doc = new XmlDocument();
+                res_doc.LoadXml(ret_str);
+                return res_doc.DocumentElement.GetElementsByTagName("digiEAR_indexCloseHandleResult").Item(0).InnerText;
+            }
+        }
+
+
+        public string digiEAR_searchOpenHandle(out int ihSearch)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            string ret_str = CallWebService("http://" + site + "/digiEARservices/DEsearch.asmx", "digiEAR_searchOpenHandle", parameters, new NetworkCredential(userName, password, site));
+            if (ret_str == "")
+            {
+                ihSearch = 0;
+                return "web_services_error";
+            }
+            else
+            {
+                XmlDocument res_doc = new XmlDocument();
+                res_doc.LoadXml(ret_str);
+                ihSearch = System.Int32.Parse(res_doc.DocumentElement.GetElementsByTagName("ihSearch").Item(0).InnerText);
+                return res_doc.DocumentElement.GetElementsByTagName("digiEAR_searchOpenHandleResult").Item(0).InnerText;
+            }
+        }
+
+
+        public DataSet digiEAR_search(int maxResults, DataSet searchDS, int hSearch)
+        {
+            string xsd = "";
+            using (StringWriter sw = new StringWriter())
+            {
+                searchDS.WriteXml(sw, XmlWriteMode.WriteSchema);
+                xsd = sw.ToString();
+            }
+            XmlDocument xsd_doc = new XmlDocument();
+            xsd_doc.LoadXml(xsd);
+            string schema = xsd_doc.GetElementsByTagName("xs:schema").Item(0).OuterXml;
+            string diffgram = "";
+            using (StringWriter sw = new StringWriter())
+            {
+                searchDS.WriteXml(sw, XmlWriteMode.DiffGram);
+                diffgram = sw.ToString();
+            }
+            string final = "<searchDS>" + schema + "\n" + diffgram.Insert(139, " xmlns=\"\"") + "</searchDS>";
+            //MessageBox.Show(final);
+            
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("imaxResults", maxResults.ToString());
+            parameters.Add("", final);
+            parameters.Add("ihSearch", hSearch.ToString());
+            string ret_str = CallWebService("http://" + site + "/digiEARservices/DEsearch.asmx", "digiEAR_search2", parameters, new NetworkCredential(userName, password, site));
+            //string ret_str = "";
+            DataSet hitDS = new DataSet();
+            if (ret_str == "")
+            {
+                return hitDS;
+            }
+            else
+            {
+                XmlDocument res_doc = new XmlDocument();
+                res_doc.LoadXml(ret_str);
+                string ret_xsd = res_doc.DocumentElement.GetElementsByTagName("digiEAR_search2Result").Item(0).OuterXml;
+                using (StringReader SR = new StringReader(ret_xsd))
+                {
+                    hitDS.ReadXml(SR);
+                }
+                return hitDS;
+            }
+        }
+
+        public string digiEAR_searchCloseHandle(int hSearch)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("ihSearch", hSearch.ToString());
+            string ret_str = CallWebService("http://" + site + "/digiEARservices/DEsearch.asmx", "digiEAR_searchCloseHandle", parameters, new NetworkCredential(userName, password, site));
+            if (ret_str == "")
+            {
+                return "web_services_error";
+            }
+            else
+            {
+                XmlDocument res_doc = new XmlDocument();
+                res_doc.LoadXml(ret_str);
+                return res_doc.DocumentElement.GetElementsByTagName("digiEAR_searchCloseHandleResult").Item(0).InnerText;
+            }
+        }
+
+        private static string CallWebService(string url, string action, Dictionary<string, string> parameters, NetworkCredential nc)
+        {
+            string xmlns = "http://cyberasiatech.com/digiEAR/XmlWebServices/";
+            string Out = String.Empty;
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+                webRequest.Headers.Add("SOAPAction", xmlns + action);
+                webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+                webRequest.Accept = "text/xml";
+                webRequest.Method = "POST";
+
+                CredentialCache myCredCache = new CredentialCache();
+                //myCredCache.Add(new Uri(Url), "Negotiate", (NetworkCredential)CredentialCache.DefaultCredentials);
+                myCredCache.Add(new Uri(url), "Negotiate", nc);
+
+                // Set the username and the password.
+                webRequest.Credentials = myCredCache;
+                // This property must be set to true for Kerberos authentication.
+                webRequest.PreAuthenticate = true;
+                // Keep the connection alive.
+                webRequest.KeepAlive = true;
+                webRequest.UnsafeAuthenticatedConnectionSharing = true;
+                webRequest.UserAgent = "Upload Test";
+                webRequest.AllowWriteStreamBuffering = false;
+                webRequest.Timeout = 10000;
+
+                webRequest.AllowWriteStreamBuffering = true;
+
+                XmlDocument soapEnvelopeXml = CreateSoapEnvelope(xmlns, action, parameters);
+                InsertSoapEnvelopeIntoWebRequest(soapEnvelopeXml, webRequest);
+
+                
+                HttpWebResponse WResponse = (HttpWebResponse)webRequest.GetResponse();
+                // Read your response data here.
+                System.IO.Stream ReceiveStream = WResponse.GetResponseStream();
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(ReceiveStream, Encoding.UTF8))
+                {
+                    Out = sr.ReadToEnd();
+                }
+                // Close all streams.
+                WResponse.Close();
+                
+                /*
+                // begin async call to web request.
+                IAsyncResult asyncResult = webRequest.BeginGetResponse(null, null);
+
+                // suspend this thread until call is complete. You might want to
+                // do something usefull here like update your UI.
+                asyncResult.AsyncWaitHandle.WaitOne();
+
+                // get the response from the completed web request.
+                using (WebResponse webResponse = webRequest.EndGetResponse(asyncResult))
+                {
+                    using (StreamReader rd = new StreamReader(webResponse.GetResponseStream()))
+                    {
+                        Out = rd.ReadToEnd();
+                    }
+                }
+                 * */
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(string.Format("HTTP_ERROR :: The second HttpWebRequest object has raised an Argument Exception as 'Connection' Property is set to 'Close' :: {0}", ex.Message));
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show(string.Format("HTTP_ERROR :: WebException raised! :: {0}", ex.Message));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("HTTP_ERROR :: Exception raised! :: {0}", ex.Message));
+            }
+            return Out;
+        }
+
+        private static XmlDocument CreateSoapEnvelope(string xmlns, string action, Dictionary<string, string> parameters)
+        {
+            XmlDocument soapEnvelop = new XmlDocument();
+
+            string namespaces = "http://schemas.xmlsoap.org/soap/envelope/";
+            XmlElement env = soapEnvelop.CreateElement("soap", "Envelope", namespaces);
+            env.SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+            env.SetAttribute("xmlns:xsd", "http://www.w3.org/2001/XMLSchema");
+            env.SetAttribute("xmlns:soap", namespaces);
+
+            //XmlNodeList body = soapEnvelop.GetElementsByTagName("soap:Body");
+            XmlElement body = soapEnvelop.CreateElement("soap", "Body", namespaces);
+            XmlElement aa = soapEnvelop.CreateElement(action);
+            aa.SetAttribute("xmlns", xmlns);
+            foreach (KeyValuePair<string, string> entry in parameters)
+            {
+                if (entry.Key != "")
+                {
+                    XmlElement pp = soapEnvelop.CreateElement(entry.Key);
+                    pp.InnerText = entry.Value;
+                    aa.AppendChild(pp);
+                }
+                else
+                {
+                    XmlDocumentFragment fragment = soapEnvelop.CreateDocumentFragment();
+                    fragment.InnerXml = entry.Value;
+                    aa.AppendChild(fragment);
+                }
+            }
+            body.AppendChild(aa);
+            env.AppendChild(body);
+            soapEnvelop.AppendChild(env);
+
+            return soapEnvelop;
+        }
+
+        private static void InsertSoapEnvelopeIntoWebRequest(XmlDocument soapEnvelopeXml, HttpWebRequest webRequest)
+        {
+            using (Stream stream = webRequest.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+        }
+
+        private static string xmlDocument_to_string(XmlDocument xmlDoc)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                using (XmlTextWriter tx = new XmlTextWriter(sw))
+                {
+                    xmlDoc.WriteTo(tx);
+                    return sw.ToString();
+                }
+            }
+        }
+
+    }
+}
---
+++ digiEAR_API_SOAP_sample/digiEAR_API_SOAP_sample/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace digiEAR_API_SOAP_sample
+{
+    public partial class Form1 : Form
+    {
+        DE_API de_api = new DE_API();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.media_path.Text = this.folderBrowserDialog1.SelectedPath;
+            }   
+        }
+
+        private void import_medias_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(this.media_path.Text)) return;
+
+            if (de_api.digiEAR_indexAllowed() < 0) return;
+            this.listBox1.Items.Add("index_allow_ok");
+
+            DirectoryInfo di = new DirectoryInfo(this.media_path.Text);
+            try
+            {
+                foreach (var fi in di.EnumerateFiles())
+                {
+                    if (fi.Extension == ".wmv" || fi.Extension == ".wav" || fi.Extension == ".mp3" || fi.Extension == ".wma")
+                    {
+                        index(fi.DirectoryName, fi.Name);
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException DirNotFound) {
+                this.listBox1.Items.Add(DirNotFound.Message);
+            }
+            catch (UnauthorizedAccessException UnAuthDir) {
+                this.listBox1.Items.Add(UnAuthDir.Message);
+            }
+            catch (PathTooLongException LongPath) {
+                this.listBox1.Items.Add(LongPath.Message);
+            }
+        }
+
+
+        private void index(string dir_path, string file_name)
+        {
+            int hIndex = 0;
+            try
+            {
+                //
+                // get a “indexing handle”(hIndex) for given acoustic model
+                // from server for "indexing"
+                //
+                string acoName = "Aco_Tele_NAEnglish";
+                string iError = de_api.digiEAR_indexOpenHandle(acoName, out hIndex);
+                if (iError != "DE_SUCCESS") throw (new ApplicationException("digiEAR_indexOpenHandle" + iError.ToString()));
+                if (hIndex == 0) throw (new ApplicationException("Unable to get handle: " + iError));
+                this.listBox1.Items.Add("hIndex = " + hIndex.ToString());
+
+                //
+                // Start indexing for the given media file, acoustic model
+                // the .pat file and media file will be moved to patDir after
+                // indexing
+                //
+                string medFile = dir_path + "\\" + file_name;
+                string patDir = dir_path;
+                this.listBox1.Items.Add("medFile = " + medFile);
+                this.listBox1.Items.Add("patDir = " + patDir);
+
+                iError = de_api.digiEAR_index(medFile, acoName, patDir, hIndex);
+                if (iError != "DE_SUCCESS") throw (new ApplicationException("digiEAR_index" + iError.ToString()));
+
+                Thread.Sleep(3000);
+
+                // close the handle
+                //
+                iError = de_api.digiEAR_indexCloseHandle(hIndex);
+                this.listBox1.Items.Add(iError.ToString());
+
+                this.listBox1.Items.Add("index end !!");
+
+            }
+            catch (Exception e1)
+            {
+                this.listBox1.Items.Add(e1.Message);
+                if (0 < hIndex) de_api.digiEAR_indexCloseHandle(hIndex);
+            }
+        }
+
+        private void add_pat_file(DataTable patDT, string patFilePath)
+        {
+            DataRow workRow = patDT.NewRow();
+            workRow["Pat"] = patFilePath;
+            patDT.Rows.Add(workRow);
+        }
+
+        private void add_term(DataTable queryDT, int minScore, string term)
+        {
+            DataRow workRow = queryDT.NewRow();
+            workRow["MinScore"] = minScore;
+            workRow["QueryTerm"] = term;
+            queryDT.Rows.Add(workRow);
+        }
+
+        private void select_term_file_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.term_file.Text = this.openFileDialog1.FileName;
+            }
+        }
+
+        private void search_btn_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(this.media_path.Text)) return;
+            if (!File.Exists(this.term_file.Text)) return;
+            if (!Directory.Exists(this.output_path.Text)) return;
+
+            if (de_api.digiEAR_indexAllowed() < 0) return;
+            
+            //
+            // searchDS is part of input
+            // hitDS is the result of "search"
+            //
+            DataSet searchDS = new DataSet("SearchDS");
+            DataSet hitDS = new DataSet("HitDS");
+            int hSearch = 0;
+
+            try
+            {
+                //
+                // prepare DataTable patDT
+                //
+                DataTable patDT = searchDS.Tables.Add("PatDT");
+                patDT.Columns.Add("Pat", typeof(string));
+                DirectoryInfo di = new DirectoryInfo(this.media_path.Text);
+                try
+                {
+                    foreach (var fi in di.EnumerateFiles())
+                    {
+                        if (fi.Extension == ".wmv" || fi.Extension == ".wav" || fi.Extension == ".mp3" || fi.Extension == ".wma")
+                        {
+                            string patFilePath = fi.FullName + ".pat";
+                            if (!File.Exists(patFilePath)) 
+                            {
+                                index(fi.DirectoryName, fi.Name);
+                            }
+                            add_pat_file(patDT, patFilePath);
+                        }
+                    }
+                }
+                catch (DirectoryNotFoundException DirNotFound)
+                {
+                    Console.WriteLine("(0)", DirNotFound.Message);
+                }
+                catch (UnauthorizedAccessException UnAuthDir)
+                {
+                    Console.WriteLine("(0)", UnAuthDir.Message);
+                }
+                catch (PathTooLongException LongPath)
+                {
+                    Console.WriteLine("(0)", LongPath.Message);
+                }
+
+                //
+                // prepare DataTable queryDT
+                //
+                DataTable queryDT = searchDS.Tables.Add("QueryDT");
+                queryDT.Columns.Add("MinScore", typeof(int));
+                queryDT.Columns.Add("QueryTerm", typeof(string));
+                String[] terms = File.ReadAllText(this.term_file.Text).Split('\n');
+                foreach (var term in terms)
+                {
+                    //Console.WriteLine("{0}", term);
+                    char[] separators = { ',', '"' };
+                    String[] components = term.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                    if (components.Length >= 2)
+                    {
+                        add_term(queryDT, Convert.ToInt32(components[1]), components[0]);
+                    }
+                }
+
+                //
+                // max. # of hits to be returned from server: maxResults
+                //
+                int maxResults = 100;
+                string deError = "DE_SUCCESS";
+                //
+                // get a "handle" (hSearch) from server for "search"
+                //
+                deError = de_api.digiEAR_searchOpenHandle(out hSearch);
+                if (deError != "DE_SUCCESS") throw (new ApplicationException("digiEAR_searchOpenHandle :" + deError.ToString()));
+                if (hSearch == 0) throw (new ApplicationException("Unable to get handle: " + deError));
+                this.listBox1.Items.Add("hSearch = " + hSearch.ToString());
+                //
+                // request Web Service
+                //
+                hitDS = de_api.digiEAR_search(maxResults, searchDS, hSearch);
+                //
+                // close the handle
+                //
+                deError = de_api.digiEAR_searchCloseHandle(hSearch);
+                if (deError != "DE_SUCCESS") throw (new ApplicationException("digiEAR_searchCloseHandle :" + deError.ToString()));
+                this.listBox1.Items.Add("Status: " + deError.ToString());
+
+
+                result_grid_view.DataSource = hitDS.Tables["HitDT"];
+
+                string output = string.Empty;
+                output += "key word, media file name, starting time, ending time, score \n";
+                if (0 < hitDS.Tables.Count)
+                {
+                    for (int i = 0; i < hitDS.Tables["HitDT"].Rows.Count; ++i)
+                    {
+                        
+                        output += hitDS.Tables["HitDT"].Rows[i][4].ToString() + ", ";
+                        string pat_name = hitDS.Tables["HitDT"].Rows[i][1].ToString();
+                        output += pat_name.Substring(0, pat_name.Length - 4) + ", ";
+                        output += hitDS.Tables["HitDT"].Rows[i][2].ToString() + ", ";
+                        output += hitDS.Tables["HitDT"].Rows[i][3].ToString() + ", ";
+                        output += hitDS.Tables["HitDT"].Rows[i][0].ToString() + "\n";
+                        
+                        /*
+                        for (int j = 0; j < hitDS.Tables["HitDT"].Columns.Count; ++j)
+                        {
+                            output += hitDS.Tables["HitDT"].Rows[i][j].ToString();
+                            output += ",";
+                        }
+                        output += "\n";                        
+                         * */
+                        
+                    }
+                }
+                else
+                {
+                    this.listBox1.Items.Add("No Search Results");
+                }
+                this.listBox1.Items.Add(output);
+                File.WriteAllText(this.output_path.Text + "/result.csv", output);
+                
+                this.listBox1.Items.Add("search end !!");
+
+            }
+            catch (Exception e1)
+            {
+                //txbxMessage.Text = e1.Message;
+                this.listBox1.Items.Add(e1.Message);
+                //if (0 < hSearch) ws.digiEAR_searchCloseHandle(hSearch);
+            }
+
+            //
+            // dispose DataSet's
+            //
+            searchDS.Dispose();
+            hitDS.Dispose();
+            //ws.Dispose();
+        }
+
+        private void select_output_path_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                this.output_path.Text = this.folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        private void clear_list_box_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            de_api = new DE_API(this.serverSite_tb.Text, this.userName_tb.Text, this.Password_tb.Text);
+        }
+    }
+}
